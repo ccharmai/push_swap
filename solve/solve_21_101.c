5ba04d2fa84a4625e1d41d721d712067dee9335f
Sergey  Kozhevnikov <sergeykozhevnikov@MacBook.local>
2020-05-08 10:44:15 +0300

    less than point

--- solve/solve_21_101.c
+++ solve/solve_21_101.c
@@ -6,17 +6,41 @@
 /*   By: ccharmai <5429549@mail.ru>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 22:21:24 by ccharmai          #+#    #+#             */
-/*   Updated: 2020/04/30 20:22:55 by ccharmai         ###   ########.fr       */
+/*   Updated: 2020/05/01 15:25:22 by ccharmai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/header.h"
 
+void	less_than_2_b(int point, t_stack **a, t_stack **b)
+{
+	int	len;
+	int	i;
+
+	i = 0;
+	len = stack_len(*a);
+	while (i < len)
+	{
+		if ((*a)->element < point)
+			push(&(*a), &(*b));
+		rotate(&(*a));
+		i++;
+	}
+}
+
 void	solve_21_101(t_stack **a)
 {
-	int *points;
+	int		*points;
+	t_stack	*b;
 
+	b = NULL;
 	points = find_3_points(*a);
-	ft_printf("general points = %i %i %i\n", points[0], points[1], points[2]);
+	// less_than_2_b(points[0], &(*a), &b);
+
+	// print_stack(b);
+	solve_6_20(&(*a));
+	print_both_stack(*a, b);
+	// print_stack(b);
+
 	free(points);
 }
